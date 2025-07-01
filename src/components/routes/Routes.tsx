041e656276aff087ec5ac3386bb51370bc160e66
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-07-01 12:20:37 +0300

    bug fixes

--- src/components/routes/Routes.tsx
+++ src/components/routes/Routes.tsx
@@ -1,31 +1,26 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import {routes} from "./list";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { routes } from './list';
 
-export const Routes = () => {
-
+export const AppRoutes = () => {
     const isAuth = true;
 
-    return(
+    return (
         <Router>
-            <Switch>
-                {
-                    routes.map(route => {
-                        if(route.auth && !isAuth){
-                            return false;
-                        }
+            <Routes>
+                {routes.map(route => {
+                    if (route.auth && !isAuth) {
+                        return null;
+                    }
 
-                        return (
-                            <Route
-                                path={route.path}
-                                exact={route.exact}
-                                key={`route ${route.path}`}
-                            >
-                                <route.component />
-                            </Route>
-                        )
-                    })
-                }
-            </Switch>
+                    return (
+                        <Route
+                            path={route.path}
+                            element={<route.component />}
+                            key={`route-${route.path}`}
+                        />
+                    );
+                })}
+            </Routes>
         </Router>
-    )
-}
+    );
+};
