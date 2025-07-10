6f599510ace4921861b2ead6608c557c6e7e7139
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-07-10 12:31:23 +0300

    added Header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,12 +1,21 @@
-import logoImage from './pngwing.com.png';
+import logoImage from '../../../assets/pngwing.com.png';
+import Search  from '@mui/icons-material/Search';
+import styles from './Header.module.css';
+import {useState} from "react";
 
 export const Header = () => {
+
+    const [isSearchActive, setIsSearchActive] = useState(false);
+
     return(
-        <header  className='header'>
-            <img src={logoImage} alt="" />
-            <input type="text" />
+        <header  className={styles.header}>
+            <div className={styles['image-wrapper']}>
+                <img src={logoImage} alt="" />
+            </div>
+            <div className={styles.wrapper}>
+                {!isSearchActive && <Search />}
+                <input type="text" placeholder="Поиск" onClick={() => setIsSearchActive(true)}/>
+            </div>
         </header>
     )
 }
-
-//TODO: останавилась на 1:25:13
