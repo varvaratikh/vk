d4f826c8f4913e012ce05709d65f0fae2381efc6
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-07-12 11:24:48 +0300

    added firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {AppRoutes} from "./components/routes/AppRoutes";
+import { AppRoutes } from "./components/routes/AppRoutes";
+ import firebase from 'firebase/app'
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBS81cK9jJxAVrZmOYLVsdD769KbQIu8o4",
+    authDomain: "vk-copy-529da.firebaseapp.com",
+    projectId: "vk-copy-529da",
+    storageBucket: "vk-copy-529da.appspot.com",
+    messagingSenderId: "587410756706",
+    appId: "1:587410756706:web:b1e488626cd9b7ac75ad3b"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+interface IContext{
+
+}
+
+const FireBaseContext = createContext<IContext>(null)
+
+const auth= firebase.auth()
+const fireStore = firebase.firestore()
+
+
+
+
+
+
+
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-      <AppRoutes />
-  </React.StrictMode>
+    <React.StrictMode>
+        <AppRoutes />
+    </React.StrictMode>
 );
-
-
